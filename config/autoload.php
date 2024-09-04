12ad078c1181193c14fc4b55516a211fcbdda66e
Maksim Zaykov <8859590+mszaykov@users.noreply.github.com>
2024-09-04 15:59:06 +0200

    autoload.php

--- config/autoload.php
+++ config/autoload.php
@@ -58,6 +58,7 @@ require_once $mvCorePath.'imager.class.php';
 require_once $mvCorePath.'content.class.php';
 require_once $mvCorePath.'record.class.php';
 require_once $mvCorePath.'paginator.class.php';
+require_once $mvCorePath.'admin_panel.class.php';
 
 $mvConfigFiles = [
 	$mvIncludePath.'config/setup.php',
