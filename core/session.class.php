05d2f09a48c7158f39bc797478be43aac1765e21
Maksim Zaykov <8859590+mszaykov@users.noreply.github.com>
2024-11-11 17:06:55 +0100

    Added initial session class.

---
+++ core/session.class.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * 
+ */
+class Session
+{
+    private static $current = null;
+
+    private static $containers = [];
+
+    private function __construct() {}
+
+    static public function start(string $container = 'front')
+    {
+        self::$current = $container;
+
+        self::$containers[$container] ??= [
+            'key' => self::generateKey($container),
+            'ip' => $_SERVER['REMOTE_ADDR'],
+            'time' => time(),
+            'data' => []
+        ];
+
+        //Debug::pre(get_class_vars(Session::class));
+        Debug::pre(self::$containers);
+        Debug::pre($_SESSION);
+    }
+
+    static public function switch(string $container = 'front')
+    {
+        if(!array_key_exists($container, self::$containers))
+            Debug::displayError("Unable to switch on '".$container."' session container. Such container was not started before. ");
+    }
+
+    static public function destroy(string $container)
+    {
+
+    }
+
+    static private function generateKey(string $container): string
+    {
+        return '';
+    }
+
+    static public function current(): ?string
+    {
+        return self::$current;
+    }
+
+    static public function dump(): array
+    {
+        return [];
+    }
+
+    static public function set(string $key, mixed $value)
+    {
+
+    }
+
+    static public function get(string $key, mixed $default = null)
+    {
+
+    }
+
+    static public function empty(string $container)
+    {
+
+    }
+}
