385d93563037e3e583b3c9234f203cc4f25684b9
Maksim Zaikov <8859590+makscraft@users.noreply.github.com>
2026-02-16 19:22:35 +0700

    Date datatype reworked and added wrong date checking.

--- core/datatypes/date.type.php
+++ core/datatypes/date.type.php
@@ -9,22 +9,36 @@ class DateModelElement extends CharModelElement
 	
 	protected $now_on_create = true;
 	
-	protected $selects_values = array("dd" => "", "mm" => "", "yyyy" => "");
+	protected $selects_values = ['dd' => '', 'mm' => '', 'yyyy' => ''];
 	
-	protected $years = false;
+	protected $years;
 	
 	protected $format = true;
 
 	public function validate()
 	{
-		parent :: validate();
-		
-		if(get_class($this) == "DateModelElement")
-			if(!$this -> display_selects && $this -> value && !I18n :: checkDateFormat($this -> value))
-				$this -> error = $this -> chooseError("format", "{error-date-format}");
-			else if($this -> display_selects && $this -> value )
-				if(!$this -> selects_values["dd"] || !$this -> selects_values["mm"] || !$this -> selects_values["yyyy"])
-					$this -> error = $this -> chooseError("required", "{error-date-values}");
+		parent::validate();
+
+		if(get_class($this) == 'DateModelElement')
+		{
+			if(!$this -> display_selects && $this -> value && !I18n::checkDateFormat($this -> value))
+				$this -> error = $this -> chooseError('format', '{error-date-format}');
+			else if($this -> display_selects && $this -> value)
+				if(!$this -> selects_values['dd'] || !$this -> selects_values['mm'] || !$this -> selects_values['yyyy'])
+					$this -> error = $this -> chooseError('required', '{error-date-values}');
+
+			if(!$this -> error && $this -> value)
+			{
+				$parts = explode('-', I18n::dateForSQL($this -> value));
+
+				if(false === checkdate(intval($parts[1]), intval($parts[2]), intval($parts[0])))
+					$this -> error = $this -> chooseError('format', '{error-undefined-value}');
+
+				if(!$this -> error && null !== $years = $this -> getYearsRange())
+					if(intval($parts[0]) < $years['from'] || intval($parts[0]) > $years['to'])
+						$this -> error = $this -> chooseError('format', '{error-undefined-value}');
+			}
+		}
 		
 		return $this;
 	}
@@ -36,9 +50,9 @@ class DateModelElement extends CharModelElement
 		$css_class = ($data['type'] == "date") ? "form-date-field" : "form-date-time-field";
 
 		$html = "<table class=\"admin-date-time-filter\"><tr><td>\n";
-		$html .= I18n :: locale('date-from')."</td><td><input type=\"text\" class=\"".$css_class."\" ";
+		$html .= I18n::locale('date-from')."</td><td><input type=\"text\" class=\"".$css_class."\" ";
 		$html .= "name=\"".$this -> name."-from\" value=\"".$value_from."\" /></td></tr><tr><td>\n";
-		$html .= I18n :: locale('date-to')."</td><td><input type=\"text\"class=\"".$css_class."\" ";
+		$html .= I18n::locale('date-to')."</td><td><input type=\"text\"class=\"".$css_class."\" ";
 		$html .= "name=\"".$this -> name."-to\" value=\"".$value_to."\" />\n";
 		$html .= "</td></tr></table>\n";
 
@@ -55,38 +69,56 @@ class DateModelElement extends CharModelElement
 	{
 		return $this -> display_selects;
 	}
+
+	public function getYearsRange(bool $forced = false)
+	{
+		if(!is_string($this -> years) && !is_array($this -> years))
+			return $forced ? ['from' => 1920, 'to' => intval(date('Y'))] : null;
+		
+		$years = is_string($this -> years) ? explode(',', $this -> years) : $this -> years;
+
+		if(isset($years[0], $years[1]) && is_numeric($years[0]) && is_numeric($years[1]) && intval($years[0]) < intval($years[1]))
+			return [
+				'from' => abs(intval($years[0])),
+				'to' => abs(intval($years[1]))
+			];
+
+		return null;
+	}
 	
 	public function setValue($value)
 	{
 		if(!$value)
 		{
-			$this -> value = "";
-			$this -> selects_values = array("dd" => "", "mm" => "", "yyyy" => "");
+			$this -> value = '';
+			$this -> selects_values = ['dd' => '', 'mm' => '', 'yyyy' => ''];
 			
 			return $this;
 		}
 		
 		if($this -> display_selects)
 		{
-			if(!preg_match("/^\d{4}-\d{2}-\d{2}/", $value))
-				$date = $value = I18n :: dateForSQL($value);
+			if(!preg_match('/^\d{4}-\d{2}-\d{2}/', $value))
+				$date = $value = I18n::dateForSQL($value);
 			else
 				$date = $value;
 			
-			$date = preg_replace("/^(\d{4}-\d{2}-\d{2}).*/", "$1", $date);
-			$date = explode("-", $date);
+			$date = preg_replace('/^(\d{4}-\d{2}-\d{2}).*/', '$1', $date);
+			$date = explode('-', $date);
 			
 			if(isset($date[0], $date[1], $date[2]) && $date[2] >= 1 && $date[2] <= 31 && $date[1] >= 1 && $date[1] <= 12)
 			{
-				$this -> selects_values = array("dd" => intval($date[2]),
-												"mm" => intval($date[1]),
-												"yyyy" => intval($date[0]));
+				$this -> selects_values = [
+					'dd' => intval($date[2]),
+					'mm' => intval($date[1]),
+					'yyyy' => intval($date[0])
+				];
 				
-				$this -> value = I18n :: dateFromSQL($value);
+				$this -> value = I18n::dateFromSQL($value);
 			}
 		}
 		else
-			parent :: setValue($value);
+			parent::setValue($value);
 		
 		return $this;
 	}
@@ -96,14 +128,14 @@ class DateModelElement extends CharModelElement
 		if(array_key_exists($key, $this -> selects_values))
 		{
 			if(!$this -> value)
-				if(get_class($this) == "DateModelElement")
-					$this -> value = I18n :: getDateFormat();
+				if(get_class($this) == 'DateModelElement')
+					$this -> value = I18n::getDateFormat();
 				else
-					$this -> value = I18n :: getDateTimeFormat().":00";
+					$this -> value = I18n::getDateTimeFormat().':00';
 				
-			$this -> selects_values[$key] = ($value != "") ? intval($value) : "";
+			$this -> selects_values[$key] = ($value != '') ? intval($value) : '';
 
-			$value = ($value < 10) ? "0".$value : $value; 
+			$value = ($value < 10) ? '0'.$value : $value; 
 			
 			$this -> value = str_replace($key, $value, $this -> value);
 		}
@@ -115,12 +147,12 @@ class DateModelElement extends CharModelElement
 	{
 		$arguments = func_get_args();
 		
-		if(isset($arguments[0]) && $arguments[0] == "sql" && !is_null($this -> value))
+		if(isset($arguments[0]) && $arguments[0] == 'sql' && !is_null($this -> value))
 		{
-			if(preg_match("/^\d{4}-\d{2}-\d{2}(\s\d{2}:\d{2})?(:\d{2})?$/", $this -> value))
+			if(preg_match('/^\d{4}-\d{2}-\d{2}(\s\d{2}:\d{2})?(:\d{2})?$/', $this -> value))
 			  	return $this -> value; 
 			else
-				return I18n :: dateForSQL($this -> value);
+				return I18n::dateForSQL($this -> value);
 		}
 		else
 			return $this -> value;
@@ -132,10 +164,10 @@ class DateModelElement extends CharModelElement
 			return $this -> displayAsSelects();
 		else
 		{
-			$html = parent :: displayHtml();
+			$html = parent::displayHtml();
 			
-			if(strpos($html, "form-text-input"))
-				return str_replace("form-text-input", "form-text-input form-date-field", $html);
+			if(strpos($html, 'form-text-input'))
+				return str_replace('form-text-input', 'form-text-input form-date-field', $html);
 			
 			return str_replace("<input", "<input class=\"form-date-field\"", $html);
 		}
@@ -144,10 +176,10 @@ class DateModelElement extends CharModelElement
 	public function displayAsSelects()
 	{
 		$value = $this -> selects_values['dd'];
-		$error_class = ($value == "") ? " not-selected" : " selected";
+		$error_class = ($value == '') ? ' not-selected' : ' selected';
 		
 		$html = "<select name=\"select-dd-".$this -> name."\" class=\"form-day-select".$error_class."\">\n";
-		$html .= "<option value=\"\">".I18n :: locale('day')."</option>\n";
+		$html .= "<option value=\"\">".I18n::locale('day')."</option>\n";
 		
 		for($i = 1; $i <= 31; $i ++)
 		{
@@ -158,39 +190,28 @@ class DateModelElement extends CharModelElement
 		$html .= "</select>\n";
 		
 		$value = $this -> selects_values['mm'];
-		$error_class = ($value == "") ? " not-selected" : " selected";
+		$error_class = ($value == '') ? ' not-selected' : ' selected';
 		
 		$html .= "<select name=\"select-mm-".$this -> name."\" class=\"form-month-select".$error_class."\">\n";
-		$html .= "<option value=\"\">".I18n :: locale('month')."</option>\n";
+		$html .= "<option value=\"\">".I18n::locale('month')."</option>\n";
 		
 		for($i = 1; $i <= 12; $i ++)
 		{
 			$selected = ($value && $value == $i) ? " selected=\"selected\"" : "";
-			$html .= "<option value=\"".$i."\"".$selected.">".I18n :: getMonth($i)."</option>\n";
+			$html .= "<option value=\"".$i."\"".$selected.">".I18n::getMonth($i)."</option>\n";
 		}
 		
 		$html .= "</select>\n";
 		
 		$value = $this -> selects_values['yyyy'];
-		$error_class = ($value == "") ? " not-selected" : " selected";
+		$error_class = ($value == '') ? ' not-selected' : ' selected';
 		
 		$html .= "<select name=\"select-yyyy-".$this -> name."\" class=\"form-year-select".$error_class."\">\n";
-		$html .= "<option value=\"\">".I18n :: locale('year')."</option>\n";
+		$html .= "<option value=\"\">".I18n::locale('year')."</option>\n";
 		
-		$years = $this -> years ? explode(",", $this -> years) : false;
+		$years = $this -> getYearsRange(true);
 		
-		if(isset($years[0], $years[1]) && $years[0] < $years[1] && $years[0] && $years[1])
-		{
-			$year_from = intval($years[0]);
-			$year_to = intval($years[1]);
-		}
-		else
-		{
-			$year_from = 1920;
-			$year_to = intval(date("Y"));
-		}
-		
-		for($i = $year_to; $i >= $year_from; $i --)
+		for($i = $years['to']; $i >= $years['from']; $i --)
 		{
 			$selected = ($value && $value == $i) ? " selected=\"selected\"" : "";
 			$html .= "<option value=\"".$i."\"".$selected.">".$i."</option>\n";
@@ -198,6 +219,6 @@ class DateModelElement extends CharModelElement
 		
 		$html .= "</select>\n";
 		
-		return (get_class($this) == "DateModelElement") ? $html.$this -> addHelpText() : $html;
+		return (get_class($this) == 'DateModelElement') ? $html.$this -> addHelpText() : $html;
 	}
 }
