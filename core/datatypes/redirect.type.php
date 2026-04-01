0ed6004cde6dccaebeb7cab85a7291ea62d7d7fd
Maksim Zaikov <8859590+makscraft@users.noreply.github.com>
2026-04-01 16:22:59 +0700

    Redirect datatype reworked.

--- core/datatypes/redirect.type.php
+++ core/datatypes/redirect.type.php
@@ -4,17 +4,16 @@
  */
 class RedirectModelElement extends CharModelElement
 {
-	protected $format = "/^https?:\/\/(www\.)?(([a-z\d-]+\.)+[a-z]{2,4}|localhost)((\/|\?).*)*$/";
+	protected $format = '/^https?:\/\/(www\.)?(([a-z\d-]+\.)+[a-z]{2,4}|localhost)((\/|\?).*)*$/';
 	
 	public function validate()
 	{
 		$arguments = func_get_args();
-		parent :: validate($arguments[0], $arguments[1]);
-		$this -> value = mb_strtolower($this -> value, "utf-8");
+		parent::validate($arguments[0] ?? null, $arguments[1] ?? null);
 		
 		if(!$this -> error && $this -> value)
 			if(!preg_match($this -> format, $this -> value))
-				$this -> error = $this -> chooseError("format", "{error-redirect-format}");
+				$this -> error = $this -> chooseError('format', '{error-redirect-format}');
 		
 		return $this;
 	}
